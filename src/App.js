e9ef901cf67e29c9bd4666856d1c773822b3011b
Connor Reese <connor.reese13@gmail.com>
2020-02-28 17:09:34 +0800

    added input events

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import React, { Component } from 'react'
-import Chat from './Chat'
-import './styles/App.css'
+import React, { Component } from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import "./styles/App.css";
+import Chat from "./Chat";
+import Login from "./Login";
+import Signup from "./Signup";
 
 class App extends Component {
-	// Methods
-	// Render
-	render() {
-		return <Chat />
-	}
+  // Methods
+  // Render
+  render() {
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route path="/login" component={Login} />
+          <Route path="/signup" component={Signup} />
+          <Route path="/" component={Chat} />
+        </Switch>
+      </BrowserRouter>
+    );
+  }
 }
 
-export default App
+export default App;
--- src/Signup.js
+++ src/Signup.js
@@ -1,27 +1,53 @@
-import React from 'react'
+import React from "react";
 
 class Signup extends React.Component {
-	// Data
-	state = {}
-	// Methods
-	signup = () => {}
-	// Render
-	render() {
-		return (
-			<form className="card" onSubmit={this.signup}>
-				<input type="text" placeholder="Full Name" />
-				<input type="text" placeholder="Email" />
-				<input type="password" placeholder="Password" />
-				<button type="submit" className="positive">
-					Signup
-				</button>
-				<div className="link">
-					<a href="/login">Already have an account? Login</a>
-				</div>
-				<div className="error">{this.state.error}</div>
-			</form>
-		)
-	}
+  // Data
+  state = {
+    name: "",
+    email: "",
+    password: ""
+  };
+  // Methods
+  signup = e => {
+    e.preventDefault();
+    this.props.history.push("/");
+  };
+  changeInput = (e, field) => {
+    let x = this.state;
+    x[field] = e.target.value;
+    this.setState(x);
+
+    console.log(this.state);
+  };
+  // Render
+  render() {
+    return (
+      <form className="card" onSubmit={this.signup}>
+        <input
+          type="text"
+          placeholder="Full Name"
+          onChange={e => this.changeInput(e, "name")}
+        />
+        <input
+          type="text"
+          placeholder="Email"
+          onChange={e => this.changeInput(e, "email")}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          onChange={e => this.changeInput(e, "password")}
+        />
+        <button type="submit" className="positive">
+          Signup
+        </button>
+        <div className="link">
+          <a href="/login">Already have an account? Login</a>
+        </div>
+        <div className="error">{this.state.error}</div>
+      </form>
+    );
+  }
 }
 
-export default Signup
+export default Signup;
